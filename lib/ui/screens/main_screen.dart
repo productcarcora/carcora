97779b43188487e0c6b11f9cf65c02860bec2f49
vinoth92.r@gmail.com <vinoth@bdo.one>
2019-07-16 16:40:23 +0800

    Tabs final

--- lib/ui/screens/main_screen.dart
+++ lib/ui/screens/main_screen.dart
@@ -2,10 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:login/business/auth.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:login/models/user.dart';
+import './tab_screens.dart';
 
 class MainScreen extends StatefulWidget {
   final FirebaseUser firebaseUser;
-
+  final String title = "Carcora";
   MainScreen({this.firebaseUser});
 
   _MainScreenState createState() => _MainScreenState();
@@ -13,51 +14,39 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
-
+  int _currentIndex = 0;
+  final List<Widget> _children = [];
+  List<Widget> tabs = [
+    TabScreen(Colors.green),
+    TabScreen(Colors.orange),
+    TabScreen(Colors.blue),
+    TabScreen(Colors.black),
+    TabScreen(Colors.red),
+  ];
   @override
   void initState() {
     super.initState();
     print(widget.firebaseUser);
   }
 
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: DefaultTabController(
-        length: 3,
-        child: Scaffold(
-          key: _scaffoldKey,
-          appBar: new AppBar(
-            bottom: TabBar(
-              tabs: [
-                Tab(icon: Icon(Icons.directions_car)),
-                Tab(icon: Icon(Icons.directions_transit)),
-                Tab(icon: Icon(Icons.directions_bike)),
-              ],
-            ),
-            title: Text('Carcora'),
-            centerTitle: true,
-          ),
-          drawer: Drawer(
-            child: ListView(
-              padding: EdgeInsets.zero,
-              children: <Widget>[
-                DrawerHeader(
-                  child: Text('Drawer Header'),
-                ),
-                ListTile(
-                  title: Text('Log Out'),
-                  onTap: () {
-                    _logOut();
-                    _scaffoldKey.currentState.openEndDrawer();
-                  },
-                ),
-              ],
-            ),
-          ),
-          body: TabBarView(
-            children: [
-              StreamBuilder(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: StreamBuilder(
                 stream: Auth.getUser(widget.firebaseUser.uid),
                 builder: (BuildContext context, AsyncSnapshot<User> snapshot) {
                   if (!snapshot.hasData) {
@@ -87,18 +76,53 @@ class _MainScreenState extends State<MainScreen> {
                           ),
                           Text("Name: ${snapshot.data.firstName}"),
                           Text("Email: ${snapshot.data.email}"),
-                          Text("UID: ${snapshot.data.userID}"),
+                          // Text("UID: ${snapshot.data.userID}"),
                         ],
                       ),
                     );
                   }
                 },
               ),
-              Icon(Icons.directions_transit),
-              Icon(Icons.directions_bike),
-            ],
-          ),
+            ),
+            ListTile(
+              title: Text('Log Out'),
+              onTap: () {
+                _logOut();
+                _scaffoldKey.currentState.openEndDrawer();
+              },
+            ),
+          ],
         ),
+      ),
+      body: tabs[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        onTap: onTabTapped,
+        currentIndex: _currentIndex,
+        fixedColor: Colors.blue,
+        backgroundColor: Colors.white,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text("Home"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.card_travel),
+            title: Text("Travel"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            title: Text("Profile"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.mail),
+            title: Text("Mail"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.room_service),
+            title: Text("Service"),
+          ),
+        ],
       ),
     );
   }
